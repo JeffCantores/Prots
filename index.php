b2979bf30adf83aed49a6ccee09c7deb0e828eff
JeffCantores <jeffersoncantores13@gmail.com>
2020-06-05 23:00:48 +0800

    Made index page

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require_once('view-comp/header.php'); ?>
+
+<h1>HELLO, MY HEAVEN'S BLESSING!!!</h1>
+
+
+<?php require_once('view-comp/footer.php') ?>
---
+++ view-comp/header.php
@@ -0,0 +1,14 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+    crossorigin="anonymous">
+
+    <script src="https://kit.fontawesome.com/e4cf9541ab.js" crossorigin="anonymous"></script>
+
+    <title>PROTS - My Heaven's Blessing</title>
+  </head>
+  <body>
